ad98b20ee30248ce9246fc7db27fcfbd8f800e6b
intekhab8 <ihossain@g.harvard.edu>
2024-01-11 05:39:08 +0000

    pathreg init commit

--- ode_net/code/train_inte_causal_lottery_sep.py
+++ ode_net/code/train_inte_causal_lottery_sep.py
@@ -315,7 +315,7 @@ if __name__ == "__main__":
     pruning_score_lambda_PPI = 0.05 
     pruning_score_lambda_motif = 0.01
     lr_schedule_patience = 3
-    prop_force_to_zero_for_loaded_model = 0.92
+    prop_force_to_zero_for_loaded_model = 0.83
     consider_multipliers = True
 
     odenet = ODENet(device, data_handler.dim, explicit_time=settings['explicit_time'], neurons = settings['neurons_per_layer'], 
