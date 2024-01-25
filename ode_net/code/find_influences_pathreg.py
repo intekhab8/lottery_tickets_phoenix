7e345164a3066cc32647de0e04496691b6babeab
intekhab8 <ihossain@g.harvard.edu>
2024-01-25 15:40:54 +0000

    inferred influences pathreg

--- ode_net/code/find_influences_pathreg.py
+++ ode_net/code/find_influences_pathreg.py
@@ -56,38 +56,34 @@ if __name__ == "__main__":
                                         log_scale = settings['log_scale'],
                                         init_bias_y = settings['init_bias_y'])
 
-    
-    nhidden = 300
-    data_dim = data_handler.dim
-    feature_layers = [initial_position(data_dim, nhidden), 
-                    ODEBlock(
-                        odefunc = L0_MLP(input_dim=data_dim, layer_dims=(nhidden,nhidden,data_dim), temperature = 2/3, my_lambda = 1), #, N=150
-                        dim = data_dim, 
-                        tol = 1e-3,
-                        method = settings['method']
-                    )]
-    pathreg_model = nn.Sequential(*feature_layers).to(device)
-    
-    
-    odenet.float()
+
     pretrained_model_file = '/home/ubuntu/lottery_tickets_phoenix/all_manuscript_models/breast_cancer/{}/best_val_model.pt'.format(MODEL_TYPE)
-    odenet.load(pretrained_model_file)
-    print(odenet)
+    pathreg_model = torch.load(pretrained_model_file)
+    print(pathreg_model)
+
 
     time_pts_to_project = torch.from_numpy(np.arange(0,1,0.1))
-    n_random_inputs_per_gene = 200
+    pathreg_model[1].set_times(time_pts_to_project)
+    n_random_inputs_per_gene = 3
     all_scores = []
     #Read in the prior matrix
-    for this_gene in range(data_handler.dim):#
-        this_init = 1*(torch.rand(n_random_inputs_per_gene,1,data_handler.dim, device = data_handler.device) - 0.5)
-        unpert_out = odeint(odenet,this_init, time_pts_to_project, method= settings['method'])
-        this_pert_col =  1*(torch.rand(n_random_inputs_per_gene,device = data_handler.device) - 0.5)
-        this_init[:,0, this_gene] = this_pert_col 
-        pert_out = odeint(odenet,this_init, time_pts_to_project, method= settings['method']) 
-        all_other_genes = [idx for idx in range(data_handler.dim) if idx != this_gene]
-        this_gene_score = torch.mean(abs(unpert_out[1:,:,:,all_other_genes] - pert_out[1:,:,:,all_other_genes]))
-        all_scores.append(this_gene_score.item())
-        print(this_gene)
+    for this_gene in tqdm(range(data_handler.dim), desc="Genes Progress"):#
+        this_gene_score_sum = 0
+        for random_input_idx in range(n_random_inputs_per_gene):
+            #print(this_gene, random_input_idx)
+            this_init = 1*(torch.rand(1,1,data_handler.dim, device = data_handler.device) - 0.5)
+            unpert_out = pathreg_model(this_init)
+            this_pert_col =  1*(torch.rand(1,device = data_handler.device) - 0.5)
+            this_init[:,0, this_gene] = this_pert_col 
+            pert_out = pathreg_model(this_init) 
+            all_other_genes = [idx for idx in range(data_handler.dim) if idx != this_gene]
+            this_gene_score_sum += torch.mean(abs(unpert_out[1:,:,all_other_genes] - pert_out[1:,:,all_other_genes])).item()
+        
+        all_scores.append(this_gene_score_sum/n_random_inputs_per_gene)
+        if this_gene % 1000 == 0: 
+            np.savetxt('/home/ubuntu/lottery_tickets_phoenix/all_manuscript_models/breast_cancer/inferred_influences/inferred_influence_{}_first_{}.csv'.format(MODEL_TYPE, this_gene+1), 
+                        all_scores, delimiter=',') 
+
 
     print("done, saving now!")
     np.savetxt('/home/ubuntu/lottery_tickets_phoenix/all_manuscript_models/breast_cancer/inferred_influences/inferred_influence_{}.csv'.format(MODEL_TYPE), 
